d8ac9652e713ddf516706979b883b4dac09a5b16
a25950086 <a25950086@gmail.com>
2022-01-15 19:23:44 +0800

    sign in

--- member.php
+++ member.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <html>
     <head>
         <meta charset="utf8"/>
@@ -14,12 +12,29 @@
                 height: auto;
                 background-color:	#7373B9;
             }
+            .body{
+                margin-left: 15%;
+                margin-top: 3%;
+                width: 70%;
+                height: 80%;
+                background-color:#f2f2ff;
+            }
             .icon{
                 padding-top: 5px;
                 padding-left: 15%;
             }
             .icon_s{
                 padding-left: 10%;
+            }
+            .icon_b{
+                padding-top: 3%;
+                padding-left: 25%;
+            }
+            h1{
+                text-align:center;
+            }
+            .login{
+                text-align:center;
             }
         </style>    
     </head>
@@ -38,6 +53,22 @@
                 <a href="member.php">
                     <img class="icon_s" src="member.png" height="50px" width="auto">
                 </a>
+            </div>
+            <div class="body">
+                <a  href="index.php"><img  class="icon_b" src="icon_b.png" alt="" width="50%" ></a>
+                <h1>登入</h1>
+                <div class="login">
+                <label for="inputEmail" class="sr-only">Email address</label>
+                <input type="text" name="email"id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                <br><br><br>
+                <label for="inputPassword" class="sr-only">Password</label>
+                <input type="password" name="password"id="inputPassword" class="form-control" placeholder="Password" required>
+                <br><br><br>
+                <a href="insert_member.php">註冊</a>
+                <button  type="submit" name="signIn">登入</button>
+                </div>
+               
+               
             </div>
         </header>
     </body>
